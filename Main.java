98bac9f16f96a0b6e1d13b7ae717fe42986928d5
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-07 13:47:16 +0000

    finished the for loop final test.

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *  For loop final test
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+                                                          
+    //make variable of start, end
+    int start, end;
+
+    //asks the user to enter a number to begin counting from
+    System.out.println("What number do you want to begin counting from?");
+    //inputs the starting interger
+    start = input.nextInt();
+
+    //asks the user to enter a number to end counting at
+    System.out.println("What number do you want to end counting at?");
+    //inputs the starting interger
+    end = input.nextInt();
+
+    //for loop that counts from beginning number to end number
+    for(start = 1; start >= 0; start++){
+      System.out.println(start);
+    if(start==end)
+      break;
+    }
   }
 }
